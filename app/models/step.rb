7474c37818e417108ca6c5779f9be8810c8daae8
LylyG <lgalarza@spoonflower.com>
2016-03-06 12:17:04 -0500

    Add possible module methods to step model.

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,21 @@
 class Step < ActiveRecord::Base
-  validates :date, presence: true 
+  validates :date, presence: true
+
+  def self.daily_total
+    start = Time.now.beginning_of_day
+    stop = Time.now.beginning_of_day
+    self.all.select {|e| e.created_at >= start && e.created_at <= stop}
+  end
+
+  def self.weekly_total
+    start = Time.now.beginning_of_week
+    stop = Time.now.end_of_week
+    self.all.select {|e| e.created_at >= start && e.created_at <= stop}
+  end
+
+  def self.current_month
+    start = Time.now.beginning_of_month
+    stop = Time.now.end_of_month
+    self.all.select {|e| e.created_at >= start && e.created_at <= stop}
+  end
 end
