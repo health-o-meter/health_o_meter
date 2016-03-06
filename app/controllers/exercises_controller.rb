1ac807e6a4835c81a9a19eabba82370e9058cdf8
LylyG <lgalarza@spoonflower.com>
2016-03-06 12:25:39 -0500

    Update exercise controller to fix broken exercises/new route

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,22 +1,74 @@
 class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:show, :edit, :update, :destroy]
+
+  # GET /exercises
+  # GET /exercises.json
   def index
+    @exercises = exercise.all
   end
 
+  # GET /exercises/1
+  # GET /exercises/1.json
   def show
   end
 
+  # GET /exercises/new
   def new
+    @exercise = exercise.new
   end
 
+  # GET /exercises/1/edit
   def edit
   end
 
+  # POST /exercises
+  # POST /exercises.json
   def create
+    @exercise = exercise.new(exercise_params)
+
+    respond_to do |format|
+      if @exercise.save
+        format.html { redirect_to @exercise, notice: 'exercise was successfully created.' }
+        format.json { render :show, status: :created, location: @exercise }
+      else
+        format.html { render :new }
+        format.json { render json: @exercise.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /exercises/1
+  # PATCH/PUT /exercises/1.json
   def update
+    respond_to do |format|
+      if @exercise.update(exercise_params)
+        format.html { redirect_to @exercise, notice: 'exercise was successfully updated.' }
+        format.json { render :show, status: :ok, location: @exercise }
+      else
+        format.html { render :edit }
+        format.json { render json: @exercise.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /exercises/1
+  # DELETE /exercises/1.json
   def destroy
+    @exercise.destroy
+    respond_to do |format|
+      format.html { redirect_to exercises_url, notice: 'exercise was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_exercise
+      @exercise = exercise.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def exercise_params
+      params.require(:exercise).permit(:amount, :date)
+    end
 end
